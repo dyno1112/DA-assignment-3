1411f2fa326cc486cbbbb5c3214637fa79814a32
dyno1112 <151719325+dyno1112@users.noreply.github.com>
2024-02-15 10:55:45 +0530

    Add files via upload

---
+++ a3seta3.py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+vd=SentimentIntensityAnalyzer()
+te=" I purchased Mango online. I am very happy with product "#positive
+print("\n",vd.polarity_scores(te))
+te=" I hate tea  "#-ve
+print("\n",vd.polarity_scores(te))
+te=" I saw movie yesterday. The animation was really good but script was ok "#neutral--mix
+print("\n",vd.polarity_scores(te))
+te=" I enjoy listning music "#positive
+print("\n",vd.polarity_scores(te))
